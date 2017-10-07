08ce9893e76e66c798900bfcf1ae4c37096524dc
David Hamilton <nova706@gmail.com>
2017-10-07 11:54:43 -0600

    Update Wolfram plugin to use spoken endpoint

--- plugins/bishop-ai-wolfram/index.js
+++ plugins/bishop-ai-wolfram/index.js
@@ -1,10 +1,9 @@
-var xml = require('libxmljs');
 var request = require('request');
 
 var Wolfram = function (config) {
 
     this.intent = [
-        {value: "Ask Wolfram *query", trigger: "wolfram.query"}
+        {value: "Ask Wolfram [Alpha] *query", trigger: "wolfram.query"}
     ];
 
     this.triggers = {
@@ -14,11 +13,11 @@ var Wolfram = function (config) {
 
             var query = data.namedValues.query || "";
 
-            this.query(appId, query, null, function (error, pods) {
+            Wolfram.query(appId, query, function (error, result) {
                 if (error) {
                     dfd.reject(error);
                 } else {
-                    dfd.resolve(pods[0].primary);
+                    dfd.resolve(result);
                 }
             });
         }
@@ -33,36 +32,16 @@ var Wolfram = function (config) {
     }
 };
 
-Wolfram.prototype.query = function (appId, query, params, cb) {
+Wolfram.query = function (appId, query, cb) {
     if (!appId) {
         return cb("Application key not set", null);
     }
 
-    params = (params instanceof Array) ? params.join('&') : (params) ? ("&" + params) : "";
-    var uri = 'http://api.wolframalpha.com/v2/query?input=' + encodeURIComponent(query) + params + '&primary=true&appid=' + appId;
+    var uri = 'http://api.wolframalpha.com/v1/spoken?i=' + encodeURIComponent(query) + '&appid=' + appId;
 
     request(uri, function (error, response, body) {
         if (!error && response.statusCode === 200) {
-            var doc = xml.parseXml(body), root = doc.root();
-
-            if (root.attr('error').value() !== 'false') {
-                var message = root.get('//error/msg').text();
-                return cb(message, null);
-            } else {
-                var pods = root.find('pod').map(function (pod) {
-                    var title = pod.attr('title').value();
-                    var subpods = pod.find('subpod').map(function (node) {
-                        return {
-                            title: node.attr('title').value(),
-                            value: node.get('plaintext').text(),
-                            image: node.get('img').attr('src').value()
-                        };
-                    });
-                    var primary = (pod.attr('primary') && pod.attr('primary').value()) === 'true';
-                    return {title: title, subpods: subpods, primary: primary};
-                });
-                return cb(null, pods);
-            }
+            return cb(null, body);
         } else {
             return cb(error, null);
         }
