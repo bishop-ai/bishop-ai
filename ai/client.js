63785cd2180840a0540f5cbe5ce8210f2297f852
David Hamilton <nova706@gmail.com>
2017-10-19 12:35:31 -0600

    Clean up closed sessions and allow credentials in CORS

--- ai/client.js
+++ ai/client.js
@@ -5,6 +5,8 @@ var Client = function (ioClient) {
     this.session = sessionService.newSession();
     this.ioClient = ioClient;
     this.ioClient.on('command', this.handleCommand.bind(this));
+    this.ioClient.on('disconnect', this.close.bind(this));
+    console.log("Client: session connected.");
 };
 
 Client.prototype.handleCommand = function (payload) {
@@ -18,28 +20,33 @@ Client.prototype.handleCommand = function (payload) {
             } else {
                 self.session.link(decoded.user);
             }
-        }
 
-        self.session.processExpression(payload.command, decoded ? decoded.user : "").then(function (result) {
-            if (result) {
-                self.emitEvent('response', {
-                    message: result.response
-                });
-            }
-        }, function (e) {
-            console.log("Client: unexpected error: " + e);
-        }, function (intermediateResponse) {
-            if (intermediateResponse) {
-                self.emitEvent('response', {
-                    message: intermediateResponse.response
-                });
-            }
-        });
+            self.session.processExpression(payload.command, decoded ? decoded.user : "").then(function (result) {
+                if (result) {
+                    self.emitEvent('response', {
+                        message: result.response
+                    });
+                }
+            }, function (e) {
+                console.log("Client: unexpected error: " + e);
+            }, function (intermediateResponse) {
+                if (intermediateResponse) {
+                    self.emitEvent('response', {
+                        message: intermediateResponse.response
+                    });
+                }
+            });
+        }
     });
 };
 
 Client.prototype.emitEvent = function (event, payload) {
     this.ioClient.emit(event, payload);
+};
+
+Client.prototype.close = function () {
+    this.session = null;
+    console.log("Client: session closed.");
 };
 
 module.exports = Client;
--- server.js
+++ server.js
@@ -32,6 +32,7 @@ var Server = function () {
     this.app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Credentials", "true");
         next();
     });
 
